c2e2eab89fda517472e0f6dd561fa6f84ae02e10
Jeremy Cimafonte <jbcima@jeremys-mbp.mynetworksettings.com>
2024-02-14 14:39:52 -0500

    Adds homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,31 @@
 import * as React from "react"
+import Nav from '../components/navigation.js';
 import { graphql } from 'gatsby'
 import "../styles/style.css"
 
 const IndexPage = ({data}) => {
   return (
     <main>
-      <p><span><span> </span>_______ _______ _______ _______ _______ _______<span> </span></span></p>
-      <p><span>|\ <span>    </span>/|\ <span>    </span>/|\ <span>    </span>/|\ <span>    </span>/|\ <span>    </span>/|\ <span>    </span>/|</span></p>
-      <p><span>| +---+ | +---+ | +---+ | +---+ | +---+ | +---+ |</span></p>
-      <p><span>| | <span>  </span>| | | <span>  </span>| | | <span>  </span>| | | <span>  </span>| | | <span>  </span>| | | <span>  </span>| |</span></p>
-      <p><span>| |<a>C</a><span>  </span>| | |<a>H</a><span>  </span>| | |<a>O</a><span>  </span>| | |<a>R</a><span>  </span>| | |<a>U</a><span>  </span>| | |<a>S</a><span>  </span>| |</span></p>
-      <p><span>| +---+ | +---+ | +---+ | +---+ | +---+ | +---+ |</span></p>
-      <p><span>|/_____\|/_____\|/_____\|/_____\|/_____\|/_____\|</span></p>
+      <Nav data={data} />
     </main>
   )
 }
 
-// export const pageQuery = graphql`
-// query pageQuery {
-//   allMarkdownRemark {
-//     edges {
-//       node {
-//       id
-//       html
-//       frontmatter {
-//         title
-//         description
-//         audio
-//       }
-//       fileAbsolutePath
-//       }
-//     }
-//   }
-// }
-// `;
+export const NavigationQuery = graphql`
+  query NavigationQuery {
+      allDirectory {
+          edges {
+              node {
+              relativeDirectory
+              relativePath
+              base
+              }
+          }
+      }
+  }
+`;
 
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home</title>
 
 export default IndexPage
