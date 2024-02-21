58b0e0e54bf3d1d001285d44499f8d94f589da84
Jeremy Cimafonte <me@jeremycimafonte.com>
2024-02-20 22:49:17 -0500

    Remove console.logs

---
+++ src/components/FileControls.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { useState, useEffect, useRef, useCallback } from 'react';
+
+
+const FileControls = ({
+  audioRef,
+  progressBarRef,
+  duration,
+  timeProgress,
+  setTimeProgress,
+  isPlaying,
+  setIsPlaying,
+  item
+}) => {
+  const url="https://or-us.ch/file/"
+
+  const togglePlayPause = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  const playAnimationRef = useRef();
+
+  const repeat = useCallback(() => {
+    const currentTime = audioRef.current.currentTime;
+    setTimeProgress(currentTime);
+    progressBarRef.current.value = currentTime;
+    progressBarRef.current.style.setProperty(
+      '--range-progress',
+      `${(progressBarRef.current.value / duration) * 100}%`
+    );
+
+    playAnimationRef.current = requestAnimationFrame(repeat);
+  }, [audioRef, duration, progressBarRef, setTimeProgress]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+    playAnimationRef.current = requestAnimationFrame(repeat);
+  }, [isPlaying, audioRef, repeat]);
+
+  return (
+    <div className="controls">
+      <p className="playButton" onClick={togglePlayPause}>{isPlaying ? 'Pause' : 'Play'}</p>
+      <p><a href={url + item.file} className="download" download>Download</a></p>
+      <br />
+    </div>
+  );
+};
+
+export default FileControls;
