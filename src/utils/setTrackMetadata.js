02d05c1d833432e8e8b790b215fdc6ac2ded5fbf
Jeremy Cimafonte <me@jeremycimafonte.com>
2024-02-21 20:11:27 -0500

    Adds track metadata from cms to audio player

---
+++ src/utils/setTrackMetadata.js
@@ -0,0 +1,10 @@
+const setTrackMetadata = (track) => {
+  navigator.mediaSession.metadata = new MediaMetadata({
+    title: track.title ? track.title : '',//t
+    artist: track.artist ? track.artist : '',
+    album: track.album ? track.album : '',
+    artwork: [{ src: track.art ? track.art : '' }],
+  });
+};
+
+export default setTrackMetadata
